9427e798481d75d430b8018be2538c3059dd9667
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-01-09 13:37:59 +0900

    fix: warning

--- src/lib.rs
+++ src/lib.rs
@@ -5,33 +5,33 @@ pub trait Spawnable {
 }
 
 pub trait SpawnableExt {
-    fn spawns(&mut self, spawnable: &mut impl Spawnable) -> EntityCommands;
+    fn spawns(&mut self, spawnable: &mut impl Spawnable) -> EntityCommands<'_>;
 }
 
 impl SpawnableExt for Commands<'_, '_> {
-    fn spawns(&mut self, spawnable: &mut impl Spawnable) -> EntityCommands {
+    fn spawns(&mut self, spawnable: &mut impl Spawnable) -> EntityCommands<'_> {
         spawnable.spawn(self)
     }
 }
 
 impl<R: Relationship> SpawnableExt for RelatedSpawnerCommands<'_, R> {
-    fn spawns(&mut self, spawnable: &mut impl Spawnable) -> EntityCommands {
+    fn spawns(&mut self, spawnable: &mut impl Spawnable) -> EntityCommands<'_> {
         spawnable.spawn(self)
     }
 }
 
 pub trait GenericBuilder {
-    fn spawn<T: Bundle>(&mut self, bundle: T) -> EntityCommands;
+    fn spawn<T: Bundle>(&mut self, bundle: T) -> EntityCommands<'_>;
 }
 
 impl GenericBuilder for Commands<'_, '_> {
-    fn spawn<T: Bundle>(&mut self, bundle: T) -> EntityCommands {
+    fn spawn<T: Bundle>(&mut self, bundle: T) -> EntityCommands<'_> {
         self.spawn(bundle)
     }
 }
 
 impl<R: Relationship> GenericBuilder for RelatedSpawnerCommands<'_, R> {
-    fn spawn<T: Bundle>(&mut self, bundle: T) -> EntityCommands {
+    fn spawn<T: Bundle>(&mut self, bundle: T) -> EntityCommands<'_> {
         self.spawn(bundle)
     }
 }
